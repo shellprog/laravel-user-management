a46e7463b4be0ecdd5b36086fb4b7feda56fefbe
Md Imran <shellprog@gmail.com>
2014-10-21 17:56:47 +0530

    Fix exceptions

---
+++ src/KodeInfo/UserManagement/Exceptions/UserSuspendedException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Imran
+ * Date: 21-10-2014
+ * Time: 05:50
+ */
+
+namespace KodeInfo\UserManagement\Exceptions;
+
+
+class UserSuspendedException extends AuthException{
+
+} 
